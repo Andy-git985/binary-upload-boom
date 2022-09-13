fb29a19b30bbcc01d9247db0902d39db15d475ab
Andy-git985 <andy.yu617@gmail.com>
2022-09-12 23:13:27 -0400

    refactored helper timeDiff

--- helpers/timeDiff.js
+++ helpers/timeDiff.js
@@ -1,37 +1,39 @@
-// function diff_hours(dt2, dt1) {
-//   var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-//   diff /= 60 * 60;
-//   return Math.abs(Math.round(diff));
-// }
+function dayDiff(createdAt) {
+  const currentDate = new Date();
+  const Difference_In_Time = currentDate - createdAt;
+  const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+  return Difference_In_Days;
+}
 
-// dt1 = new Date(2014, 10, 2);
-// dt2 = new Date(2014, 10, 3);
-// console.log(diff_hours(dt1, dt2));
+function hourDiff(createdAt) {
+  const currentDate = new Date();
+  const Difference_In_Time = currentDate - createdAt;
+  let diff = Difference_In_Time / 1000;
+  diff /= 60 * 60;
+  return diff;
+}
 
-// dt1 = new Date('October 13, 2014 08:11:00');
-// dt2 = new Date('October 13, 2014 11:13:00');
-// console.log(diff_hours(dt1, dt2));
+function postDiff(createdAt) {
+  const Difference_In_Days = dayDiff(createdAt);
+  if (Difference_In_Days < 1) {
+    const diff = hourDiff(createdAt);
+    return `${Math.abs(Math.round(diff))} HOURS AGO`;
+  } else {
+    return `${Math.trunc(Difference_In_Days)} DAYS AGO`;
+  }
+}
 
-// var date1 = new Date('06/30/2019');
-// var date2 = new Date('07/30/2019');
-
-// // To calculate the time difference of two dates
-// var Difference_In_Time = date2.getTime() - date1.getTime();
-
-// // To calculate the no. of days between two dates
-// var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+function commentDiff(createdAt) {
+  const Difference_In_Days = dayDiff(createdAt);
+  if (Difference_In_Days < 1) {
+    const diff = hourDiff(createdAt);
+    return `${Math.abs(Math.round(diff))}H`;
+  } else {
+    return `${Math.trunc(Difference_In_Days)}D`;
+  }
+}
 
 module.exports = {
-  timeDiff: (createdDate) => {
-    const currentDate = new Date();
-    const Difference_In_Time = currentDate - createdDate;
-    const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-    if (Difference_In_Days < 1) {
-      let diff = Difference_In_Time / 1000;
-      diff /= 60 * 60;
-      return `${Math.abs(Math.round(diff))} HOURS AGO`;
-    } else {
-      return `${Math.trunc(Difference_In_Days)} DAYS AGO`;
-    }
-  },
+  commentDiff: commentDiff,
+  postDiff: postDiff,
 };
